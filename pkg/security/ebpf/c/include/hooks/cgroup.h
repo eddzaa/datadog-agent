e1f10bc44a22a279949761e825d3622dc89d6a0a
Sylvain Baubeau <lebauce@gmail.com>
2024-02-27 17:03:24 +0100

    Handle cgroups created by systemd

--- pkg/security/ebpf/c/include/hooks/cgroup.h
+++ pkg/security/ebpf/c/include/hooks/cgroup.h
@@ -76,27 +76,37 @@ static __attribute__((always_inline)) int trace__cgroup_write(ctx_t *ctx) {
             return 0;
     }
 
-    char prefix[15];
-    bpf_probe_read(&prefix, sizeof(prefix), container_id);
+    char prefix[31];
+    char check_validity = 1;
+    int length = bpf_probe_read_str(&prefix, sizeof(prefix), container_id) & 0x1f;
+
     if (prefix[0] == 'd' && prefix[1] == 'o' && prefix[2] == 'c' && prefix[3] == 'k' && prefix[4] == 'e'
         && prefix[5] == 'r' && prefix[6] == '-') {
         container_id += 7; // skip "docker-"
     }
-    if (prefix[0] == 'c' && prefix[1] == 'r' && prefix[2] == 'i' && prefix[3] == 'o' && prefix[4] == '-') {
+    else if (prefix[0] == 'c' && prefix[1] == 'r' && prefix[2] == 'i' && prefix[3] == 'o' && prefix[4] == '-') {
         container_id += 5; // skip "crio-"
     }
-    if (prefix[0] == 'l' && prefix[1] == 'i' && prefix[2] == 'b' && prefix[3] == 'p' && prefix[4] == 'o'
+    else if (prefix[0] == 'l' && prefix[1] == 'i' && prefix[2] == 'b' && prefix[3] == 'p' && prefix[4] == 'o'
         && prefix[5] == 'd' && prefix[6] == '-') {
         container_id += 7; // skip "libpod-"
     }
-    if (prefix[0] == 'c' && prefix[1] == 'r' && prefix[2] == 'i' && prefix[3] == '-' && prefix[4] == 'c'
+    else if (prefix[0] == 'c' && prefix[1] == 'r' && prefix[2] == 'i' && prefix[3] == '-' && prefix[4] == 'c'
         && prefix[5] == 'o' && prefix[6] == 'n' && prefix[7] == 't' && prefix[8] == 'a' && prefix[9] == 'i'
         && prefix[10] == 'n' && prefix[11] == 'e' && prefix[12] == 'r' && prefix[13] == 'd' && prefix[14] == '-') {
         container_id += 15; // skip "cri-containerd-"
     }
 
     bpf_probe_read(&new_entry.container.container_id, sizeof(new_entry.container.container_id), container_id);
-    if (!is_container_id_valid(new_entry.container.container_id)) {
+
+    if (prefix[length-9] == '.' && prefix[length-8] == 's' && prefix[length-7] == 'e'
+            && prefix[length-6] == 'r' && prefix[length-5] == 'v' && prefix[length-4] == 'i'
+            && prefix[length-3] == 'c' && prefix[length-2] == 'e') {
+        new_entry.container.container_id[length-9] = '\0';
+        check_validity = 0;
+    }
+
+    if (check_validity && !is_container_id_valid(new_entry.container.container_id)) {
         return 0;
     }
 
